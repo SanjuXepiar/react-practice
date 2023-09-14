c733f9e7f5e9b0b73954dfde9b23c5e852290a5a
SanjuXepiar <sanjusarkar238@gmail.com>
2023-09-14 13:58:05 +0530

    updatedsetPost function
    got clear with the setState function on updating the object property

--- 12.like-list/src/App.js
+++ 12.like-list/src/App.js
@@ -4,26 +4,30 @@ import { FaThumbsUp } from "react-icons/fa";
 import React, { useState } from "react";
 
 function App() {
-  const [likedItems, setLikedItems] = useState({});
+  const [post, setPost] = useState(Data);
   const handleLike = (item) => {
-    setLikedItems((prevLikedItems) => ({
-      ...prevLikedItems,
-      [item.id]: !prevLikedItems[item.id],
-    }));
+    const updatedPost = post.map((task) => {
+      if (task.id === item.id) {
+        return {
+          ...task,
+          like: !task.like,
+        };
+      }
+      return task;
+    });
+    setPost(updatedPost);
   };
-
   return (
     <div className="App">
-      {Data.map((item) => {
+      {post.map((item) => {
         return (
           <div key={item.id}>
             <h3>
               {item.text}
               <span
-                key={item.id}
                 onClick={() => handleLike(item)}
                 style={{
-                  color: likedItems[item.id] && "blue",
+                  color: item.like && "blue",
                   cursor: "pointer",
                 }}
               >
@@ -38,3 +42,27 @@ function App() {
 }
 
 export default App;
+
+// //  const [likedItems, setLikedItems] = useState({});
+//   const handleLike = (item) => {
+//     setLikedItems((prevLikedItems) => ({
+//       ...prevLikedItems,
+//       [item.id]: !prevLikedItems[item.id],
+//     }));
+//   };
+
+{
+  /* <h3>
+{item.text}
+<span
+  key={item.id}
+  onClick={() => handleLike(item)}
+  style={{
+    color: likedItems[item.id] && "blue",
+    cursor: "pointer",
+  }}
+>
+  <FaThumbsUp />
+</span>
+</h3> */
+}
